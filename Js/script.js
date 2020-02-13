e74fa1e25eb65ab21f4e98e2a4661710e83e3e00
Kakarot99 <44390546+Kakarot99@users.noreply.github.com>
2020-02-13 22:04:52 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+   
+    $('.CMore').click(function(){
+        $('.L').toggle();
+    })
+    $(window).resize(function(){
+
+        if ($(window).width() <= 768) {  
+ 
+            $('.flip-box-front').click(function(){
+                $('.flip-box-back').show();
+            })
+ 
+        }     
+ 
+ });
+
+    
+   
+})
+
